f0a4a3906650278e62fb1caf02d0d3909e8596ab
Jenny <ricejl@gmail.com>
2019-12-07 13:11:50 -0700

    add todo toggle for completion status

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,15 +1,30 @@
 export default class Todo {
   constructor(data) {
-    this.id = data._id;
+    this._id = data._id;
     this.description = data.description;
     this.user = data.user;
     this.completed = data.completed;
   }
 
   get Template() {
-    return `
-        <li><input type="checkbox" /><label for="todo">${this.description}</label></li>
+    let template = "";
+    if (this.completed == false) {
+      template = `
+        <li><input type="checkbox" onclick="app.todoController.toggleTodoStatus('${this._id}')"/>
         `;
+    } else if (this.completed == true) {
+      template = `
+        <li><input type="checkbox" checked onclick="app.todoController.toggleTodoStatus('${this._id}')"/>
+        `;
+    }
+    template += `
+        <label for="todo">${this.description}</label>
+        <button class="btn-blank" type="button" onclick="app.todoController.removeTodoAsync(todoId)('${this._id}')">
+            <i class="fa fa-times"></i>
+        </button>
+        </li>
+        `;
+    return template;
   }
 }
 
@@ -17,7 +32,7 @@ export default class Todo {
 //   completed: { type: Boolean, required: true, default: false },
 //   user: { type: String, required: true },
 //   description: {
-// description: form.value,
-// type: String,
-// required: true
+//      description: form.value,
+//      type: String,
+//      required: true
 //   }
--- app/services/todo-service.js
+++ app/services/todo-service.js
@@ -22,13 +22,27 @@ class TodoService {
 
   async toggleTodoStatusAsync(todoId) {
     let todo = store.State.todos.find(todo => todo._id == todoId);
+    console.log("todo with matching id", todo);
     //TODO Make sure that you found a todo,
     //		and if you did find one
     //		change its completed status to whatever it is not (ex: false => true or true => false)
-    //FIXME pickup here to mark items as complete when checked
+    if (todo.completed == false) {
+      todo.completed = true;
+    } else if (todo.completed == true) {
+      todo.completed = false;
+    }
+    // FIXME how can I express this as a ternary?
+    // todo.completed == false ? todo.completed = true :
+    // todo.completed == true ? todo.completed = false
+    // FIXME possible to use .checked? How?
+    // let completeStatus = document.getElementById("check").checked;
+    // console.log("checked?", completeStatus);
 
     let res = await todoApi.put(todoId, todo);
     //TODO do you care about this data? or should you go get something else?
+    //TODO get number of items incomplete and print number to page via controller
+    console.log("response from put request for todo complete/incomplete", res);
+    this.getTodos();
   }
 
   async removeTodoAsync(todoId) {
